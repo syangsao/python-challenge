b964dd9507ea03ddd5f01fee83361397cd1b8e0b
Sam Yangsao <syangsao@redhat.com>
2019-06-15 20:20:16 -0500

    updated file data

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,31 +25,36 @@ with open(budget_csv, newline="") as csvfile:
     for row in csvreader:
         # calculate the amount of total months
         total_months.append(row[0])
-##        total_months += 1
-## https://dfrieds.com/python/read-in-csv-files
-##        print(total_months)      
-##        total_months.append(row[0])
         
         # calculate the total amount in dollars
         total_dollars += int(row[1])
-#        total_dollars.append(row[1])
-        
- ##       print(total_dollars)
-        
+                
         # what the average change was
+        change = int(row[1]) + 2  
+        average_change.append(change)
         
         # the greatest increase in profits 
+        increase = int(row[1])
+        greatest_increase.append(increase)
         
         # the greatest decrease in profits
+        decrease = int(row[1])
+        greatest_decrease.append(decrease)
 
+        # the greatest increase month
+        
+        # the greatest decrease month
+        
+        
+print(max(greatest_increase))
+print(min(greatest_decrease))
 
 ### Use print statements below as a test only ###
         
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(len(total_months)))
-print("Total Dollars: " + str(total_dollars))
-        
+print("Total: $" + str(total_dollars))       
 # print("Average Change: " + (average_change))
-# print("Greatest Increase: " + (greatest_increase))
-# print("Greatest Decrease: " + (greatest_decrease))
+print("Greatest Increase: " + "(" + max(greatest_increase) + ")")
+print("Greatest Decrease: " + "(" + min(greatest_decrease) + ")")
