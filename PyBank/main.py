ac744e727d0e539ef87c5403e0f91e2c33e9d696
Sam Yangsao <syangsao@redhat.com>
2019-06-17 18:29:39 -0500

    updated changes and final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,77 +1,68 @@
-# Import the os module
+# import modules
 import os
-# Import the cvs module
 import csv
 
-## Import operator
-## https://stackoverflow.com/questions/6193498/pythonic-way-to-find-maximum-value-and-its-index-in-a-list
-import operator
+# set the path to the file
 
-# Path to budget_data csv file
-# Note that we are in the same directory structure as this script
-budget_csv = os.path.join ('.', 'budget_data.csv')
+budget_csv = os.path.join(".","budget_data.csv")
 
-# Set the empty variables 
-total_months = []
+# variable lists 
+
+profit = []
+monthly_changes = []
+date = []
+count = 0
 total_dollars = 0
-average_change = []
-increase_greatest = []
-decrease_greatest = []
-increase_month = []
-decrease_month = []
+total_change_profits = 0
+initial_profit = 0
+
+# open csv file
 
 with open(budget_csv, newline="") as csvfile:
-    
-    # CSV reader specifies delimiter and variable that holds the contents
     csvreader = csv.reader(csvfile, delimiter=",")
-    
-    # This skips the first row (From 3-2-07)
-    csv_header = next(csvreader) 
+    csv_header = next(csvreader)
 
-    for row in csvreader:
-        # calculate the amount of total months
-        total_months.append(row[0])
-        
-        # calculate the total amount in dollars
-        total_dollars += int(row[1])
-                
-        # what the average change was
-        change = int(row[1]) + 2  
-        average_change.append(change)
-        
-        # the greatest increase in profits 
-        increase = int(row[1])
-        increase_greatest.append(increase)
-        
-        # the greatest decrease in profits
-        decrease = int(row[1])
-        decrease_greatest.append(decrease)
+    # Conducting the ask
+    for row in csvreader:    
+      # count the # of months
+      count = count + 1 
+      date.append(row[0])
 
-        # the greatest increase month
-        testmax = (max(enumerate(increase_greatest), key=operator.itemgetter(1)))
-                
-        # the greatest decrease month
-        testmin = (min(enumerate(decrease_greatest), key=operator.itemgetter(1)))
+      # append and count the profit
+      profit.append(row[1])
+      total_dollars += int(row[1])
 
-(max(increase_greatest)) #figure out the index, then apply it to the entire index/line for row[0]
-(min(decrease_greatest)) #figure out the index, then apply it to the entire index/line for row[0]
-                
-#for index, value in enumerate(increase_greatest):
-#    print(max(index, value))
+      # average change in profits from month to month and calulate the average change in profits
+      final_profit = int(row[1])
+      monthly_change_profits = final_profit - initial_profit
 
-### Use print statements below as a test only ###
-        
-print("Financial Analysis")
-print("----------------------------")
-print("Total Months: " + str(len(total_months)))
-print("Total: $" + str(total_dollars))       
-# print("Average Change: " + (average_change))
-print("Greatest Increase: " + "(" + str(max(increase_greatest)) + ")")
-print("Greatest Decrease: " + "(" + str(min(decrease_greatest)) + ")")
+      # store the monthly changes in a list
+      monthly_changes.append(monthly_change_profits)
+      total_change_profits = total_change_profits + monthly_change_profits
+      initial_profit = final_profit
 
-# Set variable for output file
-output_file = os.path.join("financial_analysis.txt")
+      # figure out the average change 
+      average_change_profits = (total_change_profits/count)
+      
+      # figure out the max and min change 
+      greatest_increase = max(monthly_changes)
+      greatest_decrease = min(monthly_changes)
+      increase_date = date[monthly_changes.index(greatest_increase)]
+      decrease_date = date[monthly_changes.index(greatest_decrease)]
+      
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(count))
+    print("Total Profits: " + "$" + str(total_dollars))
+    print("Average Change: " + "$" + str(int(average_change_profits)))
+    print("Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase) + ")")
+    print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease)+ ")")
 
-#  Open the output file
-with open(output_file, "w", newline="") as datafile:
-    writer = csv.writer(datafile)
+with open('financial_analysis.txt', 'w') as text:
+    text.write("  Financial Analysis"+ "\n")
+    text.write("----------------------------\n")
+    text.write("    Total Months: " + str(count) + "\n")
+    text.write("    Total Profits: " + "$" + str(total_dollars) +"\n")
+    text.write("    Average Change: " + '$' + str(int(average_change_profits)) + "\n")
+    text.write("    Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase) + ")\n")
+    text.write("    Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease) + ")\n")
